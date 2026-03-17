ea95e5b73214a7b3b80b8aa4242437ef4b7fae7f
Fahad Sameer Shaikh <88428777+herofahad777@users.noreply.github.com>
2026-03-18 00:12:15 +0530

    Set base path for Vite configuration

--- truesay/vite.config.ts
+++ truesay/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/UAI-hackathon-2026/',
   server: {
     allowedHosts: true,
   },
